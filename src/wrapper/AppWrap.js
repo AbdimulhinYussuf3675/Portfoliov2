3aafdf07ff0465a39e869067c50bd9776499d730
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-06-22 14:48:09 +0300

    Remove unecessary feature

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import SocialMedia from '../components/SocialMedia';
 
 const AppWrap = (Component, idName, classNames) => function HOC() {
   return (
     <div id={idName} className={`app__container ${classNames}`}>
-      <SocialMedia />
       <div className="app__wrapper app__flex">
         <Component />
       </div>
