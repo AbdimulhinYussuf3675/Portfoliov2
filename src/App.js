d42642e39f895876e2a6c2fc6ee7a5fa48f7e9fd
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-06-24 08:59:23 +0300

    Add project component route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Header from './container/Home/Home';
 import About from './container/About/About';
+import Work from './container/Project/Projects';
 
 import './App.scss';
 
@@ -11,6 +12,7 @@ const App = () => (
     <Navbar />
     <Header />
     <About />
+    <Work />
   </div>
 );
 
