fb0240530af5ac53ed9847093d16ddc371488ace
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-06-24 14:55:22 +0300

    Add skills component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Header from './container/Home/Home';
 import About from './container/About/About';
 import Work from './container/Project/Projects';
+import Skills from './container/Skills/Skills';
 
 import './App.scss';
 
@@ -13,6 +14,7 @@ const App = () => (
     <Header />
     <About />
     <Work />
+    <Skills />
   </div>
 );
 
